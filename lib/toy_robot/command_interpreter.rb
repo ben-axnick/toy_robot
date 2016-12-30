2b2a5422dd6f9410fe9c5eaa285ddcb05e567257
Ben Axnick <ben+github@axnick.com.au>
2016-12-30 19:20:09 +1100

    Added specs for the command interpreter
    As suspected, testing is a bit clunky as class responsibilities are
    muddled. I'm going to finish this out end to end with one working
    command before returning to refactor it.

--- lib/toy_robot/command_interpreter.rb
+++ lib/toy_robot/command_interpreter.rb
@@ -1,13 +1,24 @@
-require 'toy_robot/commands'
+require "toy_robot/commands"
+require "toy_robot/tokenizer"
 
 module ToyRobot
-  class CommandInterpreter < Struct.new(:tokenizer)
-    def process(line)
-      tokens = tokenizer.tokenize(line)
+  class CommandInterpreter
+    def initialize(tokenizer: nil, commands: nil, fallback: nil)
+      @tokenizer = tokenizer || Tokenizer.new
+      @commands = commands   || Commands::DEFAULT
+      @fallback = fallback   || Commands::Null
+    end
 
-      Commands::DEFAULT
-        .first { |command| command.matches?(tokens.cmd) }
-        .new(*tokens.args)
+    def process(line)
+      tokens = @tokenizer.tokenize(line)
+      found = commands.detect { |command| command.matches?(tokens.cmd) }
+      found.new(*tokens.args)
+    end
+
+    private
+
+    def commands
+      @commands | [@fallback]
     end
   end
 end
