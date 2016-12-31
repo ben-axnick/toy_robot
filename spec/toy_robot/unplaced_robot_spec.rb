9d19bb1dd850fc708b93436838bf423fe3777835
Ben Axnick <ben+github@axnick.com.au>
2016-12-31 22:47:42 +1100

    Get rid of the class dependency of uplaced robot

--- spec/toy_robot/unplaced_robot_spec.rb
+++ spec/toy_robot/unplaced_robot_spec.rb
@@ -4,9 +4,9 @@ describe ToyRobot::UnplacedRobot do
   describe "#place" do
     it "returns an object that can report its location" do
       unplaced_robot = described_class.new
-      location = instance_double(ToyRobot::TablePlacement)
+      placed_robot = unplaced_robot.place(ToyRobot::TablePlacement.new(0, 0, :south))
 
-      expect(unplaced_robot.place(location)).to be_kind_of(ToyRobot::Robot)
+      expect(placed_robot.respond_to?(:table_placement)).to eq(true)
     end
   end
 end
